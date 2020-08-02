d7e6a317bf26bb4127c7e71d339c5b527ec3b0f7
shunichi <khf04440@nifty.com>
2020-08-02 20:56:55 +0900

    first commit

---
+++ components/navbar01.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-navbar type="dark" variant="info">
+    <b-navbar-brand href="#">
+      <div class="font-awesome" style="color: orange;">
+        <font-awesome-icon icon="carrot" />
+      </div>
+    </b-navbar-brand>
+    {{ brand }}
+
+    <b-nav-text left class="mx-5">
+      <div class="font-awesome" style="color: white;" right>
+        <font-awesome-icon icon="chevron-circle-left" />
+        test-left
+      </div>
+    </b-nav-text>
+
+    <b-nav-text class="ml-auto" center>
+      <h2 style="color: white;">{{ nav_title }}</h2>
+    </b-nav-text>
+
+    <b-navbar-nav class="ml-auto mx-5">
+      <b-nav-text right>
+        <div class="font-awesome" style="color: white;" right>
+          test-right
+          <font-awesome-icon icon="chevron-circle-right" />
+        </div>
+      </b-nav-text>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Navbar01Vue',
+      brand: 'NFA',
+      status: '1',
+      nav_title: 'current step',
+      prev_icon: 'chevron-circle-left',
+      next_icon: 'chevron-circle-right',
+      prev: '',
+      next: '',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <navbar01 />
+    <Nuxt />
+  </div>
+</template>
+
+<style></style>
+<script>
+import navbar01 from '../components/navbar01'
+export default {
+  components: { navbar01 },
+}
+</script>
