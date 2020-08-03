1f0adb65c765b82d3dbddb00c39cdb1e0b89bd23
shunichi <khf04440@nifty.com>
2020-08-04 08:01:04 +0900

    couchdb迷走中

--- components/navbar01.vue
+++ components/navbar01.vue
@@ -1,46 +1,69 @@
 <template>
-  <b-navbar type="dark" variant="info">
-    <b-navbar-brand href="#">
-      <div class="font-awesome" style="color: orange;">
-        <font-awesome-icon icon="carrot" />
-      </div>
-    </b-navbar-brand>
-    {{ brand }}
-
-    <b-nav-text left class="mx-5">
-      <div class="font-awesome" style="color: white;" right>
-        <font-awesome-icon icon="chevron-circle-left" />
-        test-left
-      </div>
-    </b-nav-text>
-
-    <b-nav-text class="ml-auto" center>
-      <h2 style="color: white;">{{ nav_title }}</h2>
-    </b-nav-text>
-
-    <b-navbar-nav class="ml-auto mx-5">
-      <b-nav-text right>
-        <div class="font-awesome" style="color: white;" right>
-          test-right
-          <font-awesome-icon icon="chevron-circle-right" />
+  <div class="myNav">
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand href="/">
+        <div class="font-awesome" style="color: orange;">
+          <font-awesome-icon icon="carrot" />
         </div>
-      </b-nav-text>
-    </b-navbar-nav>
-  </b-navbar>
+      </b-navbar-brand>
+
+      <b-navbar-nav left>
+        <b-nav-text class="ml-auto" left>
+          {{ brand }}
+        </b-nav-text>
+      </b-navbar-nav>
+
+      <b-navbar-nav left class="ml-5">
+        <b-nav-text class="ml-auto mr-2" left>
+          {{ prev_title }}
+        </b-nav-text>
+
+        <b-nav-text class="ml-auto" left>
+          <a :href="prev_link">
+            <div class="font-awesome" style="color: white;">
+              <font-awesome-icon icon="chevron-circle-left" />
+            </div>
+          </a>
+        </b-nav-text>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto" center>
+        <b-nav-text>
+          <h2 style="color: white;">{{ current_title }}</h2>
+        </b-nav-text>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto mx-5">
+        <b-nav-text right class="mr-2">
+          <a :href="next_link">
+            <div class="font-awesome" style="color: white;" right>
+              <font-awesome-icon icon="chevron-circle-right" />
+            </div>
+          </a>
+        </b-nav-text>
+        <b-nav-text class="ml-auto" right>
+          {{ next_title }}
+        </b-nav-text>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'Navbar01Vue',
+      name: 'myNav',
       brand: 'NFA',
       status: '1',
-      nav_title: 'current step',
       prev_icon: 'chevron-circle-left',
       next_icon: 'chevron-circle-right',
-      prev: '',
-      next: '',
+      current_title: 'current step',
+      prev_title: 'page01',
+      next_title: 'page03',
+      current_link: 'page02',
+      prev_link: 'page01',
+      next_link: 'page03',
     }
   },
 }
---
+++ pages/pouch_test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mypouch"></div>
+</template>
+
+<script>
+export default {
+  name: 'PouchTestVue',
+}
+const PouchDB = require('pouchdb')
+  data: {
+    db: new PouchDB('notes'), // db instance
+    notes: {}, // list of all notes
+    colorPallettes: [
+      '#96ceb4',
+      ' #ffeead',
+      ' #ff6f69',
+      ' #ffcc5c',
+      '#a8e6cf',
+      '#dcedc1',
+      '#ffd3b6',
+    ], // color palettes for choosing custom background
+}
+</script>
+
+<style scoped></style>
