cbbef62d8ea39da053d2dcde5fd520a986ba9041
shunichi <khf04440@nifty.com>
2020-08-06 17:09:00 +0900

    NFAファイル　import完了

---
+++ pages/pouch_import_nfa.vue
@@ -0,0 +1,25 @@
+<template>
+  <h4>this is for importing NFA data</h4>
+</template>
+
+<script>
+// const PouchDB = require('pouchdb')
+import PouchDB from 'pouchdb'
+export default {
+  name: 'PoucnImportNfa',
+}
+const dbs = ['fct', 'dri', 'pop', 'crop_national', 'crop_name']
+
+// Replicating a local database to Remote
+dbs.map(function (value, index, array) {
+  const localdb = './assets/dbs/' + value
+  console.log(localdb)
+  const remotedb = 'http://snakada:Bluecity1965@localhost:5984/' + value
+  console.log(remotedb)
+  PouchDB.replicate(remotedb, localdb)
+})
+
+console.log('Database replicated successfully')
+</script>
+
+<style scoped></style>
